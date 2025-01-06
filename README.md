7f38afd247e4942bbb6fceb02fe336fe0bb6d057
muyangye <muyangye@usc.edu>
2025-01-05 23:50:23 -0800

    clean up

--- README.md
+++ README.md
@@ -11,8 +11,7 @@
     - `client_id`: Spotify app的Client ID
     - `client_id`: Spotify app的Client secret
     - `redirect_uri`: Spotify app的Redirect URIs中任意一个
-    - `spotify_playlist_name`: 迁移过后在Spotify里的歌单名，如填已存在的歌单则把所有歌曲插入到歌单最前，否则先创建歌单再迁移
-    - `cover_image_path`: 新歌单封面图路径（图像大小必须小于256 KB），如歌单已存在则不适用，默认值为repo里的"assets/netease.png"
+    - `cover_image_path`: 新歌单封面图路径，如未改变默认值（DESIRED_SPOTIFY_PLAYLIST_COVER_IMAGE_PATH）则取用网易云歌单的封面图，因Spotify封面图大小限制，如自定义/网易云封面图大小超过256KB则降级用repo里的"assets/netease.png"
     - `netease_playlist_id`: 想要迁移的网易云音乐歌单id，可通过网易云音乐Web端歌单链接拿到，比如链接为https://music.163.com/playlist?id=123456789&userid=xxxxxxxx<span>，歌单id就是123456789</span>
 4. 命令行输入`python cli.py`
 5. 浏览器弹窗提示登录Spotify
--- netease_to_spotify.py
+++ netease_to_spotify.py
@@ -1,16 +1,14 @@
-from datetime import date
-
-import pyncm
-from pyncm import apis
-from tqdm import tqdm
-from unidecode import unidecode
 import base64
 import re
+import requests
 import spotipy
 import sys
 import yaml
-import requests
 
+from datetime import date, datetime
+from pyncm import apis
+from tqdm import tqdm
+from unidecode import unidecode
 
 DEFAULT_COVER_PATH = "assets/netease.png"
 SPOTIFY_SCOPES = "playlist-modify-public playlist-modify-private user-library-read ugc-image-upload"
@@ -40,10 +38,7 @@ class NeteaseToSpotify:
             except:
                 print("Spotify authorization failed, program terminated.")
                 sys.exit()
-            # self.spotify_playlist_name = config["spotify_playlist_name"]
             # Use netease.png as default Spotify playlist cover image
-            # self.cover_image_path = config["cover_image_path"] if config["cover_image_path"] != "DESIRED_SPOTIFY_PLAYLIST_COVER_IMAGE_PATH" else DEFAULT_COVER_PATH
-            self.spotify_playlist_name = ""
             self.cover_image_path = config["cover_image_path"]
             self.netease_playlist_id = config["netease_playlist_id"]
 
@@ -53,11 +48,18 @@ class NeteaseToSpotify:
 
         :return: None
         """
-        # Basically just retrieve all tracks' name and 1st artist in Netease's playlist
-        # and do a search using Spotify's Search API
-        netease_playlist_tracks_name_and_artist = self.get_netease_playlist_tracks_name_and_artist()
+        # Retrieve the Netease playlist's data (name, cover image url, all tracks' name and 1st artist)
+        print(f"---------- Getting Netease Cloud Music Data With Id: {self.netease_playlist_id} (this may take a few seconds) ----------")
+        netease_playlist = apis.playlist.GetPlaylistInfo(self.netease_playlist_id)
+        netease_playlist_tracks_name_and_artist = self.get_netease_playlist_tracks_name_and_artist(netease_playlist)
+        
+        # If user has not defined a custom cover image, use the cover image from the Netease playlist
+        cover_image_path = netease_playlist["playlist"]["coverImgUrl"] \
+            if self.cover_image_path == "DESIRED_SPOTIFY_PLAYLIST_COVER_IMAGE_PATH" else self.cover_image_path
+        
+        spotify_playlist_id = self.get_or_create_playlist(netease_playlist["playlist"]["name"], cover_image_path)
 
-        spotify_playlist_id = self.get_or_create_playlist()
+        # Search all tracks of the Netease playlist in Spotify
         print("---------- Inserting Songs to Spotify ----------")
         for name, artist, year in tqdm(netease_playlist_tracks_name_and_artist):
             # Delete all parentheses because whatever inside will make search return much less/no results
@@ -68,62 +70,60 @@ class NeteaseToSpotify:
             except Exception as e:
                 print(f"Spotify does not have this song's copyright: {unidecode(name)}, {unidecode(artist)}")
     
-    def get_or_create_playlist(self):
+    def get_or_create_playlist(self, name, cover_image_path):
         """
-        Get or create the playlist of name given as the value of key "spotify_playlist_names" in the config file
+        Get or create Spotify's playlist with the specified name and cover image path
 
-        :return: the playlist's Spotify ID
+        :return: the playlist's internal Spotify ID (hash)
         :rtype: str
         """
         playlist_id = None
         # First search through all current user's playlists to see if a playlist with the same name already exists
         user_playlists = self.spotify.user_playlists(self.spotify.me()["id"])
         for playlist in user_playlists["items"]:
-            if playlist["name"] == self.spotify_playlist_name:
+            if playlist["name"] == name:
                 playlist_id = playlist["id"]
                 break
         if not playlist_id:
-            playlist_id = self.create_playlist()
+            playlist_id = self.create_playlist(name, cover_image_path)
         return playlist_id
 
-    def create_playlist(self):
+    def create_playlist(self, name, cover_image_path):
         """
-        Create a playlist
+        Create a playlist with the specified name and cover image path
 
-        :return: the new playlist's Spotify ID
+        :return: the new playlist's internal Spotify ID (hash)
         :rtype: str
         """
-        print(f"Creating spotify playlist: {self.spotify_playlist_name}, {self.cover_image_path}")
-        playlist_id = self.spotify.user_playlist_create(self.spotify.me()["id"], self.spotify_playlist_name)["id"]
+        print(f"Creating spotify playlist {name} with cover image path {cover_image_path}")
+        playlist_id = self.spotify.user_playlist_create(self.spotify.me()["id"], name)["id"]
         try:
-            b64_cover_image = self.get_base64_from_image_url(self.cover_image_path) \
-                if self.cover_image_path.startswith("http") else self.get_base64_from_image_file(self.cover_image_path)
+            b64_cover_image = self.get_base64_from_image_url(cover_image_path) \
+                if cover_image_path.startswith("http") else self.get_base64_from_image_file(cover_image_path)
             if len(b64_cover_image) / 1024 > 256:
-                print(f"The Specific image is too large, using default cover image: {DEFAULT_COVER_PATH}")
+                print(f"The cover image is too large, using default cover image")
                 b64_cover_image = self.get_base64_from_image_file(DEFAULT_COVER_PATH)
 
             self.spotify.playlist_upload_cover_image(playlist_id, b64_cover_image)
         except Exception as e:
-            print(f"Failed to create Spotify playlist (can't find cover_image_path or image is too large):{e}")
+            print(f"Failed to create Spotify playlist:{e}")
             # sys.exit()
         return playlist_id
     
     def get_base64_from_image_url(self, path):
         """
-        Get the base64 representation of an image
+        Get the base64 representation of an image from url
 
         :return: base64 representation
         :rtype: str
         """
-        # filename = path.split("/")[-1]
         response = requests.get(path, stream=True)
-        # binary_fc = open(path, "rb").read()
         base64_utf8_str = base64.b64encode(response.content).decode("utf-8")
         return base64_utf8_str
 
     def get_base64_from_image_file(self, path):
         """
-        Get the base64 representation of an image
+        Get the base64 representation of an image from file
 
         :return: base64 representation
         :rtype: str
@@ -149,21 +149,13 @@ class NeteaseToSpotify:
         # Only search for the most relevant result (first)
         return self.spotify.search(query, limit=1, type="track")["tracks"]["items"][0]["id"]
     
-    def get_netease_playlist_tracks_name_and_artist(self):
+    def get_netease_playlist_tracks_name_and_artist(self, playlist):
         """
         Get all tracks' name and 1st artist in the given Netease playlist
 
         :return: list of (name, artist) pairs of all tracks in the playlist
         :rtype: list(tuple(str, str))
         """
-        print(f"---------- Getting Netease Cloud Music Data With Id: {self.netease_playlist_id} (this may take a few seconds) ----------")
-        playlist = apis.playlist.GetPlaylistInfo(self.netease_playlist_id)
-        # print(f"playlist:{playlist}")
-        # preset playlist name and cover image for spotify playlist
-        self.spotify_playlist_name = playlist["playlist"]["name"]
-        if not self.cover_image_path or self.cover_image_path == "DESIRED_SPOTIFY_PLAYLIST_COVER_IMAGE_PATH":
-            self.cover_image_path = playlist["playlist"]["coverImgUrl"]
-
         track_ids = [track_id["id"] for track_id in playlist["playlist"]["trackIds"]]
         songs = []
         # Split track_ids to pieces of length at most 1000 to avoid PyNCM API limitation
