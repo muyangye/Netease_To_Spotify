7ebea991254af7e766f6409be21fad1d1081ca8e
Lei Shi <lei.shi@axedras.com>
2025-08-19 14:19:38 +0200

    add support for bulk import

--- cli.py
+++ cli.py
@@ -1,9 +1,13 @@
+import argparse
 from netease_to_spotify import NeteaseToSpotify
 import sys
 
-def main():
+def main(ids = None):
     app = NeteaseToSpotify()
-    app.migrate()
+    app.migrate(ids)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--list", nargs="+", help="网易云音乐歌单id, 如114115123", required=False)
+    args = parser.parse_args()
+    main(args.list)
